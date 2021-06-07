b9719cc860533d35b481f79e754cd97997c3fa87
ohapkin_da <okhapkinda@gmail.com>
2021-06-07 13:51:09 +0300

    Added push ability gor huawei

--- app/src/main/java/com/easyapps/ncraftmedia/App.kt
+++ app/src/main/java/com/easyapps/ncraftmedia/App.kt
@@ -1,11 +1,19 @@
 package com.easyapps.ncraftmedia
 
 import android.app.Application
+import android.util.Log
 import com.easyapps.ncraftmedia.repository.PostRepository
 import com.easyapps.ncraftmedia.repository.PostRepositoryNetworkImpl
+import com.huawei.agconnect.config.AGConnectServicesConfig
+import com.huawei.hms.aaid.HmsInstanceId
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class App : Application() {
     companion object {
+        const val TAG = "TEST_PUSH"
+
         lateinit var postRepository: PostRepository
     }
 
@@ -13,6 +21,24 @@ class App : Application() {
         super.onCreate()
 
         postRepository = PostRepositoryNetworkImpl()
+
+        obtainToken()
+    }
+
+    private fun obtainToken() {
+        CoroutineScope(Dispatchers.IO).launch {
+            kotlin.runCatching {
+                val appId = AGConnectServicesConfig.fromContext(this@App).getString("client/app_id")
+                Log.d(TAG, "appId = $appId")
+
+                val token = HmsInstanceId.getInstance(this@App).getToken(appId, "HCM")
+                if (token.isNotEmpty()) {
+                    Log.d(TAG, "token = $token")
+                }
+            }.onFailure {
+                Log.d(TAG, "Error", it)
+            }
+        }
     }
 
 }
